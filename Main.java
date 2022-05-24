adf3e855639c9fef98c44344f94fb98d078b03fa
Daniel Prangnell <84354822+Daniel-Prangnell@users.noreply.github.com>
2022-05-24 02:37:01 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+//import statements
+
+
+//functions go here
+
+ 
+//**********Main Routine **********
+
+//set up dictionarius / lists needed to hold data
+
+//Ask user if they have used the program before & show instructions if necessary
+
+//Loop to get ticket details
+
+  //get name (can't be blank)
+
+  //get age (between 12 and 130)
+
+  //calculate ticket price
+
+  //loop to ask for snacks
+
+  //calculate snack price
+
+  //askfor payment method (and apply surcharge if necesary)
+
+//calculate total sales and profit
+
+//output data into text file
+
+
+
+}
