1ae19d4c76719f0a4a7282392bee9f5e477ca844
Daniel Prangnell <84354822+Daniel-Prangnell@users.noreply.github.com>
2022-06-22 23:34:13 +0000

    added the code to put things into a txt document. still have to do a few other thing with it. then I can do the snacks.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 //import statements
 import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList; //importing lists
+import java.io.PrintWriter; // im porting way to print things to a txt file
+import java.io.IOException;
 
 public class Main {
 //functions go here
@@ -13,10 +15,10 @@ public class Main {
   
   public String Name(String name) {
     name = "";
+    Scanner ln = getScanner();
     while (name.isBlank() == true) {
       System.out.println("please enter your name");
-      Scanner ln = getScanner();
-      name = (scanner.next());
+      name = (scanner.nextLine());
       if (name.isBlank() == false) {
         System.out.println("you entered: " + name);
         } else {
@@ -48,7 +50,7 @@ public class Main {
           }
       }catch (Exception e) {
       System.out.println("please enter a valid integer (whole number like 1, 2, 3)");
-      System.out.println("error");
+      scanner.next();
     }
   }
     System.out.println();
@@ -59,43 +61,85 @@ public class Main {
 
   
 //**********Main Routine **********
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Main m = new Main();
     int tickets = 5;
     String name = "";
+    String ExitCode = "xxx";
+    double Cost = 0;
+    double Profit = 0;
     Boolean ValidName = false;
     Boolean ValidAge = false;
     ArrayList<String> AllNames = new ArrayList<String>();
     ArrayList<Integer> AllAges = new ArrayList<Integer>();
+    ArrayList<Double> AllCosts = new ArrayList<Double>();
+    ArrayList<Double> AllProfits = new ArrayList<Double>();
     int Age = 0;
     
-    while (tickets > 0) {
+
+
+    
+    while ((tickets > 0) & (name.equals(ExitCode) == false)) {
+      
       System.out.println("There are " + tickets + " tickets left");
       name = m.Name(name);
       Age = m.Age(Age);
-      //System.out.println(Age);
-      if (Age >= 12) {
+
+
+      
+      if (130 >= Age && 12 <= Age) {
         ValidAge = true;
         }
       if (name.isBlank() == false) {
         ValidName = true;
-      }
-      if (ValidName == true) {
-        AllNames.add(name);
-      }
-      if (ValidAge == true) {
-        AllAges.add(Age);
-      }
+        }
       if (ValidAge == true && ValidName == true) {
         tickets = tickets-1;
+        if ((Age >= 12)& (Age <= 15)){
+          Cost = 7.5;
+          Profit = 2.5;
+        }
+        else if ((Age > 16) & (Age < 65)){
+          Cost = 10.5;
+          Profit = 5.5;
+        }
+        else if (Age >= 65) {
+          Cost = 6.5;
+          Profit = 1.5;
+        }
+        AllCosts.add(Cost);
+        AllProfits.add(Profit);
+        AllNames.add(name);
+        AllAges.add(Age);
         
+        int TicketsSold = AllNames.size() - 1;
+        PrintWriter out = new PrintWriter("thing.txt");
+        while (TicketsSold != -1){
+          out.println("Name: " + AllNames.get(TicketsSold));
+          out.println("Age: " + AllAges.get(TicketsSold));
+          out.println("Cost: $" + AllCosts.get(TicketsSold));
+          out.println("Profit: $" + AllProfits.get(TicketsSold));
+          out.println();
+          if (TicketsSold == 0) {out.close();} // Close the file.
+          TicketsSold--;
+        }
+        
+        
+        
+        
+      
+        
+      
       } else {
         System.out.println("***Error - ticket not sold.***");
-      }
+        }
+      
       
       
     }
     
+    System.out.println(AllCosts);
+    System.out.println(AllProfits);
     System.out.println(AllNames);
     System.out.println(AllAges);
   }
@@ -131,15 +175,4 @@ public class Main {
 
 
 
-/*
-
-class Main {
-  public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
-
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
-  }
-}*/
 
